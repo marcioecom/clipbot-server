a756fa64fb673d641f5276a1e70894041e7e9a67
Márcio Júnior <marcio.mj70@gmail.com>
2023-02-25 22:58:40 -0300

    refactor: queue consumer

--- infra/queue/consumer.go
+++ infra/queue/consumer.go
@@ -28,7 +28,30 @@ func NewConsumer(config *Config) (IConsumer, error) {
 }
 
 func (c *consumer) Consume(topic string, handler func(*kafka.Consumer, kafka.Event) error) error {
-	return c.client.Subscribe(topic, handler)
+	if err := c.client.Subscribe(topic, nil); err != nil {
+		return err
+	}
+
+	for {
+		ev := c.client.Poll(100)
+		if ev == nil {
+			continue
+		}
+
+		switch e := ev.(type) {
+		case *kafka.Message:
+			if e.TopicPartition.Error != nil {
+				zap.L().Error("kafka error", zap.Error(e.TopicPartition.Error))
+				continue
+			}
+
+			if err := handler(c.client, e); err != nil {
+				zap.L().Error("could not handle message", zap.Error(err))
+			}
+		case kafka.Error:
+			zap.L().Error("kafka error", zap.Error(e))
+		}
+	}
 }
 
 func (c *consumer) Stop() error {
